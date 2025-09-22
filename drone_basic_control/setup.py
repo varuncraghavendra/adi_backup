31f2d1087597771b29ea49781d076706314eceb1
Varun Raghavendra <varuncrstudy@gmail.com>
2025-09-22 04:24:33 -0400

    Create setup.py

---
+++ drone_basic_control/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'drone_basic_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        # Package index resource
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        # package.xml
+        ('share/' + package_name, ['package.xml']),
+        # install launch files into share/<package>/launch
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='anna.astolfi@nttdata.com',
+    description='Basilar nodes to command drone via PX4 Autopilot',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # Make sure these module names match your *.py filenames
+            'hb_node = drone_basic_control.heartbeat_node:main',
+            'arming_node = drone_basic_control.arming_node:main',
+            'takeoff_node = drone_basic_control.takeoff_node:main',
+            'landing_node = drone_basic_control.landing_node:main',
+        ],
+    },
+)
