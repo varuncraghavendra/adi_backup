ccb767f17f75e7e80d00dd0f97133a8c9a68223b
Varun Raghavendra <varuncrstudy@gmail.com>
2025-09-22 03:44:55 -0400

    Update arming_node.py

--- drone_basic_control/arming_node.py
+++ drone_basic_control/arming_node.py
@@ -1,42 +1,41 @@
 import rclpy
 from rclpy.lifecycle import LifecycleNode, LifecycleState, TransitionCallbackReturn
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
-from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleStatus
+from px4_msgs.msg import VehicleCommand, VehicleStatus
 
 
 class ArmingNode(LifecycleNode):
-    def __init__(self, drone_ns: str = "drone_1", px4_ns: str = "px4_1"):
+    def __init__(self, drone_ns: str = "drone_1", px4_ns: str = "px4_1", px4_sys_id: int = 1):
         super().__init__(f'{drone_ns}_arming_node')
         self.declare_parameter("drone_ns", drone_ns)
         self.declare_parameter("px4_ns", px4_ns)
+        self.declare_parameter("px4_sys_id", px4_sys_id)
+
         self.drone_ns = self.get_parameter("drone_ns").get_parameter_value().string_value
         self.px4_ns = self.get_parameter("px4_ns").get_parameter_value().string_value
+        self.px4_sys_id = int(self.get_parameter("px4_sys_id").value)
 
-        self.get_logger().info(f'[{self.drone_ns}] ArmingNode init for PX4 ns: {self.px4_ns}')
+        self.get_logger().info(f'[{self.drone_ns}] ArmingNode init for PX4 ns: {self.px4_ns} (sys_id={self.px4_sys_id})')
         self.vehicle_command_publisher = None
         self.timer_ = None
         self.vehicle_status = VehicleStatus()
 
     def on_configure(self, state: LifecycleState):
         self.get_logger().info("on_configure")
-
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             durability=DurabilityPolicy.TRANSIENT_LOCAL,
             history=HistoryPolicy.KEEP_LAST,
             depth=1
         )
-        # lifecycle publisher
         self.vehicle_command_publisher = self.create_lifecycle_publisher(
             VehicleCommand, f'/{self.px4_ns}/fmu/in/vehicle_command', qos_profile
         )
         self.vehicle_status_subscriber = self.create_subscription(
             VehicleStatus, f'/{self.px4_ns}/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile
         )
-
         self.timer_ = self.create_timer(0.1, self.timer_callback)
         self.timer_.cancel()
-
         return TransitionCallbackReturn.SUCCESS
 
     def on_cleanup(self, state: LifecycleState):
@@ -78,7 +77,7 @@ class ArmingNode(LifecycleNode):
         msg.param5 = params.get("param5", 0.0)
         msg.param6 = params.get("param6", 0.0)
         msg.param7 = params.get("param7", 0.0)
-        msg.target_system = 1
+        msg.target_system = self.px4_sys_id  # <-- critical for multi-drone
         msg.target_component = 1
         msg.source_system = 1
         msg.source_component = 1
@@ -87,21 +86,10 @@ class ArmingNode(LifecycleNode):
         self.vehicle_command_publisher.publish(msg)
 
     def timer_callback(self):
+        # First ensure OFFBOARD mode (heartbeat + setpoints must already be flowing from hb_node)
         if self.vehicle_status.nav_state != VehicleStatus.NAVIGATION_STATE_OFFBOARD:
-            self.get_logger().warning(
-                f'[{self.drone_ns}] Nav state {self.vehicle_status.nav_state}. Switching to OFFBOARD...')
-            # main mode = 1, submode = 6 (OFFBOARD)
+            self.get_logger().info(f'[{self.drone_ns}] Switching to OFFBOARD...')
             self.compose_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0)
         else:
+            # Then arm
             self.compose_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = ArmingNode()
-    rclpy.spin(node)
-    rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
