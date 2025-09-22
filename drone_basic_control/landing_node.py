2017fc7d15154dabadeee59a0a666489fe8fd76e
Varun Raghavendra <varuncrstudy@gmail.com>
2025-09-22 03:45:13 -0400

    Update landing_node.py

--- drone_basic_control/landing_node.py
+++ drone_basic_control/landing_node.py
@@ -5,21 +5,23 @@ from px4_msgs.msg import VehicleCommand, VehicleStatus
 
 
 class LandingNode(LifecycleNode):
-    def __init__(self, drone_ns: str = "drone_1", px4_ns: str = "px4_1"):
+    def __init__(self, drone_ns: str = "drone_1", px4_ns: str = "px4_1", px4_sys_id: int = 1):
         super().__init__(f'{drone_ns}_landing_node')
         self.declare_parameter("drone_ns", drone_ns)
         self.declare_parameter("px4_ns", px4_ns)
+        self.declare_parameter("px4_sys_id", px4_sys_id)
+
         self.drone_ns = self.get_parameter("drone_ns").get_parameter_value().string_value
         self.px4_ns = self.get_parameter("px4_ns").get_parameter_value().string_value
+        self.px4_sys_id = int(self.get_parameter("px4_sys_id").value)
 
-        self.get_logger().info(f'[{self.drone_ns}] LandingNode init for PX4 ns: {self.px4_ns}')
+        self.get_logger().info(f'[{self.drone_ns}] LandingNode init for PX4 ns: {self.px4_ns} (sys_id={self.px4_sys_id})')
         self.vehicle_command_publisher = None
         self.timer_ = None
         self.vehicle_status = VehicleStatus()
 
     def on_configure(self, state: LifecycleState):
         self.get_logger().info("on_configure")
-
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             durability=DurabilityPolicy.TRANSIENT_LOCAL,
@@ -32,10 +34,8 @@ class LandingNode(LifecycleNode):
         self.vehicle_status_subscriber = self.create_subscription(
             VehicleStatus, f'/{self.px4_ns}/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile
         )
-
         self.timer_ = self.create_timer(0.1, self.timer_callback)
         self.timer_.cancel()
-
         return TransitionCallbackReturn.SUCCESS
 
     def on_cleanup(self, state: LifecycleState):
@@ -77,7 +77,7 @@ class LandingNode(LifecycleNode):
         msg.param5 = params.get("param5", 0.0)
         msg.param6 = params.get("param6", 0.0)
         msg.param7 = params.get("param7", 0.0)
-        msg.target_system = 1
+        msg.target_system = self.px4_sys_id  # <-- critical
         msg.target_component = 1
         msg.source_system = 1
         msg.source_component = 1
@@ -87,14 +87,3 @@ class LandingNode(LifecycleNode):
 
     def timer_callback(self):
         self.compose_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = LandingNode()
-    rclpy.spin(node)
-    rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
